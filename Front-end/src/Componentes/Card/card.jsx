6e5f6cb5f4eeae8f4d197d2bdbaec44ffba0fa10
Ueslei Lima <145942952+UesleiLM@users.noreply.github.com>
2024-09-06 23:21:41 -0300

    Feat: Criação e estilização do componente card. (#3)
    * Feat:Criação e estilização do componente card.
    
    * feat:Criação e estilização do card de produtos.

---
+++ Front-end/src/Componentes/Card/card.jsx
@@ -0,0 +1,22 @@
+import iconDolar from '../../assets/iconDolar_card.svg';
+import imgProduto from '../../assets/imagem_cebola_agrios.png';
+import './card.css';
+
+function Card() {
+    return (
+        <div className='card'>
+            <div className='img-produto'>
+                <img src={imgProduto} alt="Imagem do produto" />
+            </div>
+            <div className='info-produto'>
+                <span className='nome-produto'>Onions</span>
+                <span className='valor-produto'>$20.00</span>
+            </div>
+            <div className='icon-dolar'>
+                <img src={iconDolar} alt="Icone de Dolar" />
+            </div>
+        </div>
+    )
+}
+
+export default Card
